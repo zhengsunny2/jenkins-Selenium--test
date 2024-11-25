c70bf135288c1a45b3be84867cadbc327009421c
zhengsunny2 <zhengsunny2@gmail.com>
2024-11-25 12:09:34 +0100

    test

--- test_selenium.js
+++ test_selenium.js
@@ -9,23 +9,29 @@ const { Builder, By } = require('selenium-webdriver');
     let n1 = await driver.findElement(By.id('ajout-nom'));
     let n2 = await driver.findElement(By.id('ajout-poste'));
     let button=await driver.findElement(By.id('ajouter'));
-    let rs = await driver.findElement(By.css('#liste-employes div'));
-    let lenth1 = rs.length;
 
 await n1.sendKeys('Lili');
 await n2.sendKeys('Développeur');
 await button.click();
-rs2 = await driver.findElement(By.css('#liste-employes div'));
-let lenth2 = rs2.length;
 
-if (lenth2 === lenth1+1) {
-  console.log('Test réussi!');
-} else {
-  console.log('Test échoué');
+let employesDiv = await driver.findElement(By.id('liste-employes'));
+let employesText = await employesDiv.getText();
+
+assert(employesText.includes('Lili'), 'L\'employé Lili n\'a pas été ajouté correctement.');
+
+let idInput = await driver.findElement(By.id('supprimer-id'));
+let supprimerBtn = await driver.findElement(By.id("supprimer"));
+
+await idInput.sendKeys('4');
+await supprimerBtn.click();
+
+await driver.wait(until.elementTextContains(employesDiv, 'Dupont'), 5000, 'L\'employé Dupont devrait être supprimé.');
+
+employesText = await employesDiv.getText();
+assert(!employesText.includes('Lili'), 'L\'employé Lili été supprimé correctement.');
+} finally {
+await driver.quit();
 }
-  } finally {
-    await driver.quit();
-  }
 })();
 
 
